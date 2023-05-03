5b2c64a14d38b53772c6bca04abbc2cf2d9817d6
adaskhan <baglanadaskan@gmail.com>
2023-05-03 11:53:51 +0600

    change path to CustomValidationFailed

--- hired_go/hired_go/exceptions.py
+++ hired_go/hired_go/exceptions.py
@@ -2,7 +2,7 @@ from rest_framework.views import exception_handler
 from rest_framework.response import Response
 from rest_framework import status
 
-from hired_go.jobs.views import CustomValidationFailed
+from jobs.views import CustomValidationFailed
 
 
 def custom_exception_handler(exc, context):
