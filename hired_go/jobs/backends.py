701da536fb883b0e41d30b109a996e238da59912
adaskhan <baglanadaskan@gmail.com>
2023-05-03 11:53:21 +0600

    add EmailOrUsernameAuthenticationBackend

--- hired_go/jobs/backends.py
+++ hired_go/jobs/backends.py
@@ -1,15 +1,22 @@
 from django.contrib.auth.backends import BaseBackend
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
 
-class EmailBackend(BaseBackend):
-    def authenticate(self, request, email=None, password=None, **kwargs):
-        UserModel = get_user_model()
+class EmailOrUsernameAuthenticationBackend(BaseBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
         try:
-            user = UserModel.objects.get(email=email)
-        except UserModel.DoesNotExist:
-            return None
+            user = User.objects.get(email=username)
+        except User.DoesNotExist:
+            try:
+                user = User.objects.get(username=username)
+            except User.DoesNotExist:
+                return None
 
         if user.check_password(password):
             return user
-        return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
