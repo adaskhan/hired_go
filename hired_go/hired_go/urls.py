f2be8c144f090bb5647a264965f7fec0e556524e
adaskhan <baglanadaskan@gmail.com>
2023-05-08 16:08:24 +0600

    add path hired_fast

--- hired_go/hired_go/urls.py
+++ hired_go/hired_go/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('jobs.urls')),
+    path('api/hired_fast/', include('vacancy_parse.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
